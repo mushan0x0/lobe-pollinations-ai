4054d2c2e043ff3fba1ca8b6fd10d643b60ff555
mushan0x0 <mushan0x0@gmail.com>
2023-12-10 00:01:31 +0800

    🐛 fix: Cross-origin error

--- src/pages/api/gateway.ts
+++ src/pages/api/gateway.ts
@@ -1,13 +1,5 @@
-import { createLobeChatPluginGateway } from '@lobehub/chat-plugins-gateway';
-
 export const config = {
   runtime: 'edge',
 };
 
-export default async (req: Request) => {
-  if (process.env.NODE_ENV === 'development') {
-    return createLobeChatPluginGateway()(req);
-  }
-
-  return;
-};
+export { createLobeChatPluginGateway as default } from '@lobehub/chat-plugins-gateway';
